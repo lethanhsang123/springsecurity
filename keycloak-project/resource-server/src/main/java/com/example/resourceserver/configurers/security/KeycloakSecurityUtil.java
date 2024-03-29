7fb4b7a74e3708016e2e94c85f230da6e80b0593
sang <lethanhsang14082001@gmail.com>
2024-03-29 20:13:57 +0700

    [feat][keycloak][Add keycloak configuration]

---
+++ keycloak-project/resource-server/src/main/java/com/example/resourceserver/configurers/security/KeycloakSecurityUtil.java
@@ -0,0 +1,41 @@
+package com.example.resourceserver.configurers.security;
+
+import org.keycloak.admin.client.Keycloak;
+import org.keycloak.admin.client.KeycloakBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KeycloakSecurityUtil {
+
+    Keycloak keycloak;
+
+    @Value("${keycloak.configurations.server-uri}")
+    private String serverUri;
+
+    @Value("${keycloak.configurations.realm}")
+    private String realm;
+
+    @Value("${keycloak.configurations.client-id}")
+    private String clientId;
+
+    @Value("${keycloak.configurations.grant-type}")
+    private String grantType;
+
+    @Value("${keycloak.configurations.name}")
+    private String username;
+
+    @Value("${keycloak.configurations.password}")
+    private String password;
+
+    public Keycloak getKeycloakInstance() {
+        if(keycloak == null) {
+            keycloak = KeycloakBuilder
+                    .builder().serverUrl(serverUri).realm(realm)
+                    .clientId(clientId).grantType(grantType)
+                    .username(username).password(password).build();
+        }
+        return keycloak;
+    }
+
+}
